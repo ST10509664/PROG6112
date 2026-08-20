acb081344afb58591d7593899a02745069f5d590
Bibi Ayesha Buckus <ST10509664@myemeris.edu.za>
2026-08-20 12:47:29 +0200

    Completed feature 2

--- src/main/java/com/mycompany/prog6112_assignment1_st10509664/PROG6112_Assignment1_ST10509664.java
+++ src/main/java/com/mycompany/prog6112_assignment1_st10509664/PROG6112_Assignment1_ST10509664.java
@@ -18,6 +18,9 @@ import java.nio.file.StandardCopyOption;
 public class PROG6112_Assignment1_ST10509664 {
     static int count = 0;
     static String searchID = "";
+    static String beds = "";
+    static int countInpatient = 0;
+    static int countAvailable = 0;
     //array declaration
    static String[] ID = new String[50];
    static String[] name1 = new String[50];
@@ -26,8 +29,10 @@ public class PROG6112_Assignment1_ST10509664 {
    static String[] Gender = new String[50];
    static String[] Condition = new String[50];
    static String[] Category = new String[50];
-
-                
+   
+   static String[][] availableBeds = new String[4][5]; 
+   static String[][] occupiedBeds = new String[4][5];
+   static String[][] wardLayout = new String[4][5];             
      
     public static void main(String[] args) {
       
@@ -56,7 +61,8 @@ public class PROG6112_Assignment1_ST10509664 {
         System.out.println("3) Update an existing patient's details");
         System.out.println("4) Delete a patient");
         System.out.println("5) Display all registered patients");
-        System.out.println("6) Exit application");
+        System.out.println("6) Bed Management");
+        System.out.println("7) Exit application");
         int option = input.nextInt();
         input.nextLine();
         
@@ -278,7 +284,194 @@ public class PROG6112_Assignment1_ST10509664 {
                 }
             break;
             
+            
             case 6:
+                System.out.println("--------------------------------------------------");
+                System.out.println("Please select one of the following options below");
+                System.out.println("--------------------------------------------------");
+                System.out.println("1) Allocate beds to inpatients");
+                System.out.println("2) Discharge a patient");
+                System.out.println("3) Display ward layout");
+                System.out.println("4) Display available beds");
+                System.out.println("5) Display occupied beds");
+                int option2 = input.nextInt();
+                input.nextLine();
+                
+                PROG6112_Assignment1_ST10509664 details5 = new PROG6112_Assignment1_ST10509664();
+                details5.getDetails();
+                switch(option2) {
+                    case 1: 
+                        details5.getOccupiedBeds();
+                        //allocate beds
+                        countAvailable = 0;
+                        for(int r = 0; r < 4; r++) {
+                            for(int c = 0; c < 5; c++) {
+                                if( countAvailable < countInpatient ){
+                                    countAvailable++;
+                                    occupiedBeds[r][c] = "B" + countAvailable;  
+                                }
+                            }
+                        }
+                        if(countAvailable < 20){
+                        //allocate the occupied beds in the 2D array
+                        beds = "";
+                        for(int r = 0; r < 4; r++) {
+                            String out = "";
+                            for(int c = 0; c < 5; c++) {
+                                out = out + occupiedBeds[r][c] + "\t";
+                                if(!occupiedBeds[r][c].equals("-")){
+                                    beds = beds + occupiedBeds[r][c] + " ";
+                                }
+                            }
+                        }
+                        System.out.println("Beds successfully allocated. Occupied beds updated to: " + beds);
+                        System.out.println("--------------------------------------------------");
+                        } else {
+                        System.out.println("No beds available. A new inpatient cannot be allocated a bed at this time");
+                        System.out.println("--------------------------------------------------");   
+                        }
+                    break;
+                    
+                    case 2:      
+                        //select the patient you want to discharge, and change their category to outpatient in the textfile
+                        System.out.print("Please enter a patient ID: ");
+                        searchID = input.nextLine();
+                        details5.getDetails();
+
+                        //find the correct index to delete the correct line
+                        int matchIndex3 = -1;
+                        for (int i = 0; i < count; i++) {
+                            if (ID[i].equals(searchID)) {
+                                matchIndex3 = i;
+                                break;
+                            }
+                        }
+
+                        if (matchIndex3 == -1) {
+                            System.out.println("Patient ID not found!");
+                        } else {
+                            String lineToRemove = ID[matchIndex3] + "#" + name1[matchIndex3] + "#" + name2[matchIndex3] + "#" + Age[matchIndex3] + "#" + Gender[matchIndex3] + "#" + Condition[matchIndex3] + "#" + Category[matchIndex3];
+
+                        //remove the line existing within the textfile
+                        Path originalPath = Paths.get("Patients.txt");
+                        Path tempPath = Paths.get("Patients_temp.txt");
+
+
+                        //wtite to the textfile (append)
+                        try ( BufferedWriter writeFile = new BufferedWriter(new FileWriter("Patients.txt", true))) { 
+                         writeFile.write(ID[matchIndex3] + "#" + name1[matchIndex3] + "#" + name2[matchIndex3] + "#" + Age[matchIndex3] + "#" + Gender[matchIndex3] + "#" + Condition[matchIndex3] + "#Outpatient" );
+                         writeFile.newLine();
+                         writeFile.close();
+
+                        } catch(IOException e) {
+                            System.out.println("\n--------------------------------------------------");
+                            System.out.println("An error occurred.");
+                        } 
+
+                        try (BufferedReader reader = new BufferedReader(new FileReader(originalPath.toFile()));
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(tempPath.toFile()))) {
+                        String currentLine;
+
+                        while ((currentLine = reader.readLine()) != null) {
+                            // If it's not the line to delete, write it to the temp file
+                            if (!currentLine.trim().equals(lineToRemove)) {
+                                writer.write(currentLine);
+                                writer.newLine();
+                            }
+                        }
+                        writer.close();
+                        reader.close();
+                        // Replace the original file with the temporary file
+                        Files.move(tempPath, originalPath, StandardCopyOption.REPLACE_EXISTING);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            }
+                        }         
+                        
+                        details5.getOccupiedBeds();
+                        //allocate beds
+                        countAvailable = 0;
+                        for(int r = 0; r < 4; r++) {
+                            for(int c = 0; c < 5; c++) {
+                                if( countAvailable < countInpatient - 1 ) {
+                                    countAvailable++;
+                                    occupiedBeds[r][c] = "B" + countAvailable;  
+                                }
+                            }
+                        }
+                        //allocate the occupied beds in the 2D array
+                        beds = "";
+                        for(int r = 0; r < 4; r++) {
+                            String out = "";
+                            for(int c = 0; c < 5; c++) {
+                                out = out + occupiedBeds[r][c] + "\t";
+                                if(!occupiedBeds[r][c].equals("-")){
+                                    beds = beds + occupiedBeds[r][c] + " ";
+                                }
+                          }
+                        }
+                        System.out.println("Patient successfully discharged. Occupied beds updated to: " + beds);
+                        System.out.println("--------------------------------------------------");
+                    break;
+                 
+                    case 3:
+                        //initialise display
+                        int n = 0;
+                        for(int r = 0; r < 4; r++) {
+                            for(int c = 0; c < 5; c++) {
+                                n++;
+                                wardLayout[r][c] = "B" + n;  
+                            }
+                        }
+                        //display
+                        for(int r = 0; r < 4; r++) {
+                            String out = "";
+                            for(int c = 0; c < 5; c++) {
+                                out = out + wardLayout[r][c] + "\t";
+                          }
+                            System.out.println(out);
+                        }
+                        System.out.println("--------------------------------------------------");
+                    break;
+                    
+                    case 4: 
+                        details5.getAvailableBeds();
+                        System.out.println("\n--------------------------------------------------");
+                    break;
+                    
+                    case 5: 
+                        details5.getOccupiedBeds();
+                        //allocate beds
+                        countAvailable = 0;
+                        for(int r = 0; r < 4; r++) {
+                            for(int c = 0; c < 5; c++) {
+                                if( countAvailable < countInpatient ){
+                                    countAvailable++;
+                                    occupiedBeds[r][c] = "B" + countAvailable;  
+                                }
+                            }
+                        }
+                        //allocate the occupied beds in the 2D array
+                        beds = "";
+                        for(int r = 0; r < 4; r++) {
+                            String out = "";
+                            for(int c = 0; c < 5; c++) {
+                                out = out + occupiedBeds[r][c] + "\t";
+                                if(!occupiedBeds[r][c].equals("-")){
+                                    beds = beds + occupiedBeds[r][c] + " ";
+                                }
+                            }
+                            System.out.println(out);
+                        }
+                        System.out.println("\n--------------------------------------------------");
+                    break;
+                    
+                }
+            break;
+            
+            
+            case 7:
                 System.out.println("--------------------------------------------------");
                 System.out.println("Goodbye!");
                 running = false;
@@ -321,5 +514,60 @@ public class PROG6112_Assignment1_ST10509664 {
             System.out.println("An error occurred.");
       }    
     }
+    public void getOccupiedBeds() {
+        //count the amount of inpatients
+        countInpatient = 0;
+        for(int i = 0; i < count; i++) {
+            if (Category[i].equals("Inpatient" ) || Category[i].equals("inpatient")){
+                countInpatient++;
+            }
+        }
+        //initialize the array
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 5; c++) {
+                occupiedBeds[r][c] = "-";
+            }
+        }
+    }
     
+    public void getAvailableBeds() {
+        //count the amount of inpatients
+        countInpatient = 0;
+        for(int i = 0; i < count; i++) {
+            if (Category[i].equals("Inpatient" ) || Category[i].equals("inpatient")){
+                countInpatient++;
+            }
+        }
+        //initialize the array
+        countAvailable = 0;
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 5; c++) {
+                countAvailable++;
+                occupiedBeds[r][c] = "B" + countAvailable; 
+            }
+        }
+        //allocate beds
+        countAvailable = 0;
+        for(int r = 0; r < 4; r++) {
+           for(int c = 0; c < 5; c++) {
+               if( countAvailable < countInpatient ){
+                   countAvailable++;
+                   occupiedBeds[r][c] = "-";  
+               }
+           }
+        }
+        //allocate the occupied beds in the 2D array
+        beds = "";
+        for(int r = 0; r < 4; r++) {
+           String out = "";
+           for(int c = 0; c < 5; c++) {
+               out = out + occupiedBeds[r][c] + "\t";
+               if(!occupiedBeds[r][c].equals("-")){
+                   beds = beds + occupiedBeds[r][c] + " ";
+               }
+           }
+           System.out.println(out);                            
+        }
+    }
+
 }
